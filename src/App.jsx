f59e7a8f9ed5e3179214e9392f8bfeb5e986d744
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-02 14:14:40 +0200

    Project set up - components file and Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-import inputLine from './components/inputLine'
+
+import Header from './components/Header'
+import Card from './components/Card'
+//import InputLine from './components/InputLine'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const poulettesListe = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    
+  ];
 
   return (
-    <>
-   <card />
-       
-    </>
-  )
+    <div>
+      <Card poulette={poulettesListe} />
+      <Header />
+      <Card />
+    </div>
+
+
+    );
+
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <div>
+        <h1>Code Qui Peut/ La Boulangerie Du Code</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
